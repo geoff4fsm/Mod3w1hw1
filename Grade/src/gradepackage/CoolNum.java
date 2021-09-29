58aaa672d0afff0df70c4969baca67ad74bc78dc
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-09-29 14:50:07 -0400

    Add files via upload

--- Grade/src/gradepackage/CoolNum.java
+++ Grade/src/gradepackage/CoolNum.java
@@ -3,9 +3,9 @@ package gradepackage;
 public class CoolNum {
 
 	public static void main(String[] args) {
-		int num = 25;
+		int num = 20;
 		String numCool = null; 
-		numCool = num % 2>0 ? "Cool" : num < 5 ? "Not Cool" : num<20 ? "Cool" : "Not Cool";
+		numCool = num % 2>0 ? "Cool" : num < 5 ? "Not Cool" : num<=20 ? "Cool" : "Not Cool";
 System.out.println(numCool);
 	}
 
